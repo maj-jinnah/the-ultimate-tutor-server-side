3f361c74590c4934fd9df05b32c346fed2b8128d
maj-jinnah <maj.jinnah@gmail.com>
2022-10-25 19:03:17 +0600

    index js start

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const res = require('express/lib/response');
+app.use(cors());
+
+const Port = process.env.Port || 5000;
+
+app.get("/", (req, res) =>{
+    res.send("Now server is running!");
+})
+
+app.listen(Port, () =>{
+    console.log(`server is running, ${Port}`);
+});
