4a42165f57614373d0eed92628766da0c54c10db
maj-jinnah <maj.jinnah@gmail.com>
2022-10-25 21:59:02 +0600

    index js work done

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const res = require('express/lib/response');
+const cors = require("cors");
 app.use(cors());
 
 const Port = process.env.Port || 5000;
 
+const courses = require("./Data/fakedata.json");
+
 app.get("/", (req, res) =>{
     res.send("Now server is running!");
-})
+});
+
+app.get("/courses", (req, res) =>{
+    res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+    const id = req.params.id;
+
+    const singleCourse = courses.find( course => course.id == id);
+    res.send(singleCourse);
+});
 
 app.listen(Port, () =>{
     console.log(`server is running, ${Port}`);
